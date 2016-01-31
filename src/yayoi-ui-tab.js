55f11e426da06c3c776bfdba2be167a46cfa11a6
ChenMx <ChenMx@ChenMx-PC>
2016-01-31 23:10:25 +0800

    yayoi-tabs ui update

--- src/yayoi-ui-tab.js
+++ src/yayoi-ui-tab.js
@@ -14,9 +14,9 @@ yayoi.util.extend("yayoi.ui.tab.URLTabNode", "yayoi.ui.common.SubComponent", [],
         if(!this.title){
             throw new Error("No title to create tab node");
         }
-        this.tab = $('<li>'+this.title+'<a class="closeTab"></a></li>');
         this.frame = $('<iframe src="'+this.src+'"scrolling="auto" frameborder="0"></iframe>');
 
+        this.tab = $('<li>'+this.title+'<a class="closeTab"></a></li>');
         var closeButton = this.tab.find(".closeTab");
         if(this.closeable){
             closeButton.bind("click", function(event){
@@ -43,13 +43,13 @@ yayoi.util.extend("yayoi.ui.tab.URLTabNode", "yayoi.ui.common.SubComponent", [],
     }
     this.active = function(){
         this.parent.deactiveAll();
-        this.tab.removeClass("active").addClass("deactive");
-        this.frame.removeClass("visited");
+        this.tab.addClass("active").removeClass("deactive");
+        this.frame.addClass("active").removeClass("deactive");
         this.isActive = true;
     }
     this.deactive = function(){
-        this.tab.addClass("active").removeClass("deactive");
-        this.frame.addClass("visited");
+        this.tab.removeClass("active").addClass("deactive");
+        this.frame.removeClass("active").addClass("deactive");
         this.isActive = false;
     }
     this.close = function(){
@@ -61,54 +61,74 @@ yayoi.util.extend("yayoi.ui.tab.URLTabNode", "yayoi.ui.common.SubComponent", [],
 });
 
 yayoi.util.extend("yayoi.ui.tab.URLTab", "yayoi.ui.common.Component", [], function(){
-    this.tabContainer;
-    this.frameContainer;
+    this.navBar;
+    this.contentContainer;
     this.tabNodes = {};
 
     this.onRendering = function() {
         var container = this.getContainer();
 
-        this.tabContainer = $('<ul></ul>');
-        this.frameContainer = $('<div><div>');
-        this.tabContainer.addClass("yayoi-ui-tabs-nav");
-        this.frameContainer.addClass("yayoi-ui-tabs-main");
+        this.navBar = $('<ul></ul>');
+        this.contentContainer = $('<div><div>');
+        this.navBar.addClass("yayoi-ui-tabs-nav");
+        this.contentContainer.addClass("yayoi-ui-tabs-main");
 
         container.addClass("yayoi-ui-tabs");
-        container.append(this.tabContainer);
-        container.append(this.frameContainer);
-
-        this.resize();
+        container.append(this.navBar);
+        container.append(this.contentContainer);
 
         var tabs = this.tabs;
         for(var i=0; i<tabs.length; i++) {
-            var tab = new yayoi.ui.tab.URLTabNode(tabs[i]);
-            this.addTabNode(tab);
+            this.addTab(tabs[i]);
         }
     };
     this.resize = function(){
         var container = this.getContainer();
         var frameHeight = container.height() - 40;
-        this.frameContainer.height(frameHeight + "px");
+        this.contentContainer.height(frameHeight + "px");
     };
-    this.getTabNode = function(code) {
+    this.getTab = function(code) {
         return this.tabNodes[code];
     };
     this.deactiveAll = function(){
         for(var p in this.tabNodes){
             this.tabNodes[p].deactive();
         }
-    }
-    this.addTabNode = function(tabNode) {
-        if(this.tabNodes[tabNode.code]){
-            this.tabNodes[tabNode.code].active();
+    };
+    this.addTab = function(tab) {
+        if(!(tab instanceof yayoi.ui.tab.URLTabNode)){
+            var tab = new yayoi.ui.tab.URLTabNode(tab);
+            this.addTab(tab);
             return;
         }
-        this.tabNodes[tabNode.code] = tabNode;
-        tabNode.parent = this;
 
-        this.tabContainer.append(tabNode.tab);
-        this.frameContainer.append(tabNode.frame);
-        tabNode.active();
+        if(this.tabNodes[tab.code]){
+            this.tabNodes[tab.code].active();
+            return;
+        }
+
+        var tabNav = $('<li>'+tab.title+'<a class="closeTab"></a></li>');
+        var closeButton = tabNav.find(".closeTab");
+        if(tabNav.closeable){
+            closeButton.bind("click", function(event){
+                tab.close();
+                tabNav.hide();
+            });
+            closeButton.show();
+        } else {
+            closeButton.hide();
+        }
+        var that = this;
+        tabNav.bind("click", function(event){
+            that.click();
+        });
+
+        this.navBar.append(tabNav);
+        tab.setContainer();
+
+        this.contentContainer.append(tab.frame);
+        tab.active();
+        this.tabNodes[tab.code] = tab;
     };
     this.refresh = function(){
         for(var p in this.tabNodes){
