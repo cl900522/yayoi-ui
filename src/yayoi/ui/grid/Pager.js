83907e65966e53a58981d92d995fe3274153a626
cl900522 <545435711@qq.com>
2016-06-01 20:26:28 +0800

    pager using icon

--- src/yayoi/ui/grid/Pager.js
+++ src/yayoi/ui/grid/Pager.js
@@ -1,7 +1,7 @@
 "use strict";
 yayoi.util.initPackages("yayoi.ui.grid");
 
-yayoi.util.extend("yayoi.ui.grid.Pager", "yayoi.ui.common.BasicComponent", [], function() {
+yayoi.util.extend("yayoi.ui.grid.Pager", "yayoi.ui.common.BasicComponent", ["yayoi.ui.common.Icon"], function() {
     this.pageSize = 10;
     this.total = 0;
     this.currentNo = 1;
@@ -11,15 +11,29 @@ yayoi.util.extend("yayoi.ui.grid.Pager", "yayoi.ui.common.BasicComponent", [], f
     this.onRendering = function() {
         var container = this.getContainer();
         var html = "<div class='yayoi-pager'>";
-        html += "<span>第一页</span>";
-        html += "<span>前一页</span>";
-        html += "<input value='1'/>";
-        html += "<span>后一页</span>";
-        html += "<span>最后一页</span>";
+        html += "<div class='yayoi-pager-icon yayoi-pager-first'>第一页</div>";
+        html += "<div class='yayoi-pager-icon yayoi-pager-previous'>前一页</div>";
+        html += "<input class='yayoi-pager-input' value='1'/>";
+        html += "<div class='yayoi-pager-icon yayoi-pager-next'>后一页</div>";
+        html += "<div class='yayoi-pager-icon yayoi-pager-last'>最后一页</div>";
         html += "</div>";
         container.html(html);
     };
+    this.afterRender = function() {
+        var container = this.getContainer();
+        var iconSize = "22px";
+        this.firstIcon = new yayoi.ui.common.Icon({icon: "double-angle-left", size: iconSize});
+        this.firstIcon.placeAt(container.find(".yayoi-pager-first"));
 
+        this.previousIcon = new yayoi.ui.common.Icon({icon: "angle-left", size: iconSize});
+        this.previousIcon.placeAt(container.find(".yayoi-pager-previous"));
+
+        this.nextIcon = new yayoi.ui.common.Icon({icon: "angle-right", size: iconSize});
+        this.nextIcon.placeAt(container.find(".yayoi-pager-next"));
+
+        this.lastIcon = new yayoi.ui.common.Icon({icon: "double-angle-right", size: iconSize});
+        this.lastIcon.placeAt(container.find(".yayoi-pager-last"));
+    };
     this.reRender = function() {
         var container = this.getContainer();
         this.totalPageNo = Math.ceil(this.total / this.pageSize);
