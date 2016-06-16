274f815b832d722c996dc2bb2931db8ad837cab8
cl900522 <545435711@qq.com>
2016-06-16 12:48:22 +0800

    icon add src for image

--- src/yayoi/ui/common/Icon.js
+++ src/yayoi/ui/common/Icon.js
@@ -20,6 +20,7 @@ yayoi.extend("yayoi.ui.common.Icon", "yayoi.ui.common.BasicComponent", [], funct
             this.placeAt(this["selector"]);
             delete this["selector"];
         }
+        yayoi.getCore().regist(this);
     };
 
     this.reset = function(sIcon, sGroup) {
@@ -64,6 +65,11 @@ yayoi.extend("yayoi.ui.common.Icon", "yayoi.ui.common.BasicComponent", [], funct
         this.invalidate();
     };
 
+    this.setSrc = function(src) {
+        this.src = src;
+        this.invalidate();
+    };
+
     this.onRendering = function() {
         var container = this.getContainer();
         var html = "<span class='yayoi-icon'/></span>";
@@ -79,8 +85,10 @@ yayoi.extend("yayoi.ui.common.Icon", "yayoi.ui.common.BasicComponent", [], funct
         iconElement.attr("icon-group", this.group)
         iconElement.css("font-size", this.size);
         iconElement.css("height", this.size);
+        iconElement.css("width", this.size);
         iconElement.addClass("icon-" + this.icon);
         iconElement.css("color", this.color);
+        iconElement.css("background", "url('" + this.src + "') no-repeat");
 
         var rotateDeg = "rotate(" + this.rotate + "deg)";
         iconElement.css("-webkit-transform", rotateDeg);
