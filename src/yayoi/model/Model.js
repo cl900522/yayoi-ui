24b9675aac4c55518ffc25c02eeb202a81c43f10
cl900522 <545435711@qq.com>
2016-06-30 09:51:32 +0800

    validator refactor

--- src/yayoi/model/Model.js
+++ src/yayoi/model/Model.js
@@ -2,37 +2,30 @@
 yayoi.initPackages("yayoi.model");
 
 yayoi.extend("yayoi.model.Model", "yayoi.core.Object", ["yayoi.util.Router"], function() {
-    /*根元素的值
-     * */
-    this._rootValue;
-    /*绑定的UI组件
-     */
-    this._listeners;
-    /*获取路径下的值
-     * */
+    /*根元素的值*/
+    this._rootValue = null;
+    /*绑定的UI组件*/
+    this._listeners = null;
+    /*获取路径下的值*/
     this.getValue = function(path) {
         throw "Can not call getValue in base Model";
     };
-    /*设置路径的值
-     */
+    /*设置路径的值*/
     this.setValue = function(path, value) {
         throw "Can not call setModel in base Model";
     };
-    /*获取绑定的组件
-     */
+    /*获取绑定的组件*/
     this.getListenders = function() {
         return this._listeners;
     };
-    /*设置绑定的组件
-     */
+    /*设置绑定的组件*/
     this.addListener = function(component) {
         if (typeof(component) == "object" && component instanceof yayoi.ui.common.ModelComponent) {
             this._listeners.push(component);
             this.logger.debug("Bind component with model", component);
         }
     };
-    /*取消绑定的组件
-     */
+    /*取消绑定的组件*/
     this.removeListener = function(component) {
         for (var i = 0; i < this._listeners.length; i++) {
             if (this._listeners[i] == component) {
@@ -41,22 +34,16 @@ yayoi.extend("yayoi.model.Model", "yayoi.core.Object", ["yayoi.util.Router"], fu
             }
         }
     };
-    /*设置根节点值
-     */
+    /*设置根节点值*/
     this.setRootValue = function(value) {
         this._rootValue = value;
     };
-    /*获取根节点值
-     */
+    /*获取根节点值*/
     this.getRootValue = function() {
         return this._rootValue;
     };
     this.init = function(params) {
-        if (params && params instanceof Object) {
-            for (var p in params) {
-                this[p] = params[p];
-            }
-        }
+        yayoi.merge(this, params);
         this._listeners = [];
         this._rootValue = {};
     };
--- src/yayoi/model/Validator.js
+++ src/yayoi/model/Validator.js
@@ -2,17 +2,26 @@
 yayoi.initPackages("yayoi.model");
 
 yayoi.extend("yayoi.model.Validator", "yayoi.core.Object", [], function() {
-    this.expression = null;
+    /*Is value must be exist*/
     this.required = false;
+    this.length = null;
+    /*RegExp to test value*/
+    this.expression = null;
+    /*self validate function, return true if pass, or false if fail*/
+    this.validate = null;
+
     this.isValid = function(value) {
-        if(this.required && !value) {
-            this.onFail();
+        if (this.required && !value) {
+            return false;
         }
-        if(this.expression.test(value)) {
-            this.onFail();
+        if (this.expression && !this.expression.test(value)) {
+            return false;
+        }
+        if (this.validate) {
+            if (!this.validate(value)) {
+                return false;
+            }
         }
         return true;
-    };
-    this.onFail = function() {
     };
 });
